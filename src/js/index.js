ee7c517d52be8057464b4354f19c226dcaaa6503
Wallace Araujo <wallacearaujo.dev@gmail.com>
2022-10-10 10:35:48 -0300

    Melhoria no JavaScript, com menos if/else.

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,18 @@
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const telefoneInput = document.querySelector("#telefone");
-const textArea = document.querySelector("#mensagem");
-const submitButton = document.querySelector("#enviar");
+const botao = document.getElementById("enviar");
+const campos = document.querySelectorAll(".campo");
 
-const campoObrigatorio = document.querySelectorAll(".campo-o");
-const bordaCampoVazio = document.querySelectorAll(".campo");
-
-submitButton.addEventListener("click", function (e){
-    e.preventDefault();
-    const nameValue = nameInput.value;
-    const emailValue = emailInput.value;
-    const telefoneValue = telefoneInput.value;
-    const bigtext = textArea.value;
-
-    if(nameValue === ''){
-        campoObrigatorio[0].classList.add("obrigatorio");
-        bordaCampoVazio[0].classList.add("vazio");
-    }else{
-        bordaCampoVazio[0].classList.add("preenchido");
-    }
-
-    if(emailValue === ''){
-        campoObrigatorio[1].classList.add("obrigatorio");
-        bordaCampoVazio[1].classList.add("vazio");
-    }else{
-        bordaCampoVazio[1].classList.add("preenchido");
-    }
-
-    if(telefoneValue === ''){
-        campoObrigatorio[2].classList.add("obrigatorio");
-        bordaCampoVazio[2].classList.add("vazio");
-    }else{
-        bordaCampoVazio[2].classList.add("preenchido");
-    }
-
-    if(bigtext === ''){
-        campoObrigatorio[3].classList.add("obrigatorio");
-        bordaCampoVazio[3].classList.add("vazio");
-    }else{
-        bordaCampoVazio[3].classList.add("preenchido");
-    }
+botao.addEventListener("click", botao => {
+    botao.preventDefault();
+    const obrigatorio = document.querySelectorAll(".campo-o");
+    campos.forEach((campo, indice) => {
+        if(campo.value === ''){
+            campo.classList.remove("preenchido");
+            campo.classList.add("vazio");
+            obrigatorio[indice].classList.add("mostrar");
+        }else{
+            campo.classList.remove("vazio");
+            campo.classList.add("preenchido");
+            obrigatorio[indice].classList.remove("mostrar")
+        }
+    })
 })
